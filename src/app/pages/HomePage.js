1ebbf0c40c6efd80d688d46e1e9079f5939ec293
Sanjay Kumar <sanjaykumarwebs@gmail.com>
2025-01-04 22:46:37 +0530

    web series section added

--- src/app/pages/HomePage.js
+++ src/app/pages/HomePage.js
@@ -9,10 +9,11 @@ import MovieCardComp from '../components/MovieCardComp';
 const HomePage = () => {
 	const movieNameTags = ['patti', 'family', 'love', 'legal', 'sweet', 'sacred', 'man', 'woman', 'sex', 'loyal', 'ved', 'babygirl', 'arcane', 'murder', 'kiss', 'legal', 'hostel', 'moana', 'kathal'];
 	const [movieList, setMovieList] = useState([]);
+	const [webSeriesList, setWebSeriesList] = useState([]);
 	const [loading, setLoading] = useState(true); // Manage loading state
 	const [error, setError] = useState(null); // Manage error state
 
-	const TOTAL_MOVIE_TAGS = 10;
+	const TOTAL_MOVIE_TAGS = 5;
 
 	// Function to get random elements
 	function getRandomTags(array, num) {
@@ -55,7 +56,31 @@ const HomePage = () => {
 			}
 		};
 
+		// Fetch web series using async requests
+		const webSeries = async () => {
+			try {
+				const webSeriesPromises = getMovieTags.map((movieTag) =>
+					fetch(`https://www.omdbapi.com/?s=${movieTag}&y=2024&apikey=${process.env.NEXT_PUBLIC_API_KEY}&type=series`)
+						.then((res) => res.json())
+						.then((data) => data.Search || [])
+				);
+
+				const webSeriesArray = await Promise.all(webSeriesPromises); // Wait for all promises to resolve
+
+				// Flatten the array of web series lists and filter out invalid entries
+				const combinedWebSeries = webSeriesArray.flat().filter((webSeries) => webSeries.Poster !== 'N/A');
+
+				setWebSeriesList(combinedWebSeries); // Update the state with the combined web series list
+			} catch (err) {
+				console.error('Failed to fetch movies:', err);
+				setError('Failed to load movies. Please try again later.');
+			} finally {
+				setLoading(false); // Stop loading
+			}
+		};
+
 		fetchMovies();
+		webSeries();
 	}, []);
 
 	return (
@@ -85,7 +110,7 @@ const HomePage = () => {
 			<div className="max-w-[1800px] gap-3 grid grid-cols-12 grid-rows-2 px-8" style={{ marginTop: 30, marginBottom: 30 }}>
 				{/* Render movie list */}
 				{loading ? (
-					<div className="text-center font-medium">Loading...</div>
+					<div className="text-center font-medium">Loading Movies...</div>
 				) : error ? (
 					<div className="text-center text-red-500 font-medium">{error}</div>
 				) : movieList.length > 0 ? (
@@ -110,6 +135,58 @@ const HomePage = () => {
 					</div>
 				)}
 			</div>
+
+			<hr></hr>
+
+			<div className="px-8 mt-6 grid grid-cols-12">
+				<div className="sm:col-span-6">
+					<h2 className="text-2xl font-medium">Web Series</h2>
+				</div>
+				<div className="sm:col-span-6">
+					<Input
+						classNames={{
+							base: "max-w-full",
+							mainWrapper: "h-full",
+							input: "text-small",
+							inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+						}}
+						placeholder="Search web series"
+						size="sm"
+						startContent={<SearchIcon size={18} />}
+						type="search"
+					/>
+				</div>
+			</div>
+
+			<div className="max-w-[1800px] gap-3 grid grid-cols-12 grid-rows-2 px-8" style={{ marginTop: 30, marginBottom: 30 }}>
+				{/* Render movie list */}
+				{loading ? (
+					<div className="text-center font-medium">Loading Web Series...</div>
+				) : error ? (
+					<div className="text-center text-red-500 font-medium">{error}</div>
+				) : webSeriesList.length > 0 ? (
+					webSeriesList.map((webSeries, index) => (
+						<MovieCardComp
+							key={index}
+							isFooterBlurred
+							className="col-span-12 sm:col-span-3 md:col-span-2"
+							header={{
+								subtitle: webSeries.isNew ? "New" : null,
+								title: webSeries.Title,
+							}}
+							imageSrc={webSeries.Poster}
+							altText={webSeries.Title}
+							footerContent={<h2 className="text-black text-tiny">{webSeries.Title}</h2>}
+							onButtonClick={() => alert(`Notify me for ${webSeries.Title}`)}
+						/>
+					))
+				) : (
+					<div className="px-8 grid grid-cols-12">
+						<p className="font-medium">No web series found.</p>
+					</div>
+				)}
+			</div>
+
 		</main>
 	);
 };
